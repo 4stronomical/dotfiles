1523a4482b2d53ce7eb060b7ab215246624c6669
4stronomical <4stronomical@protonmail.com>
2022-05-29 09:24:36 +0200

    Added script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo apt install zsh polybar bspwm git terminator
+cp ./.zshrc ~/.zshrc
+git clone https://github.com/dylanaraps/wal
+sudo cp ./wal/wal /usr/bin/wal
+sudo cp -r bspwm ~/.config/
+sudo cp -r sxhkd ~/.config/
+if [[ ! -d ~/.config/polybar ]]
+then
+	mkdir ~/.config/polybar
+fi
+sudo cp -r cuts ~/.config/polybar/
+rm -rf ./wal
